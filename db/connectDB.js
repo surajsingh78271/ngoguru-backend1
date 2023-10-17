d18bcf042b2762b0eed984752869201d127f7d4c
SURAJ SINGH <84866441+surajsingh78271@users.noreply.github.com>
2023-10-17 10:34:16 +0530

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,21 +3,18 @@
 const mongoose = require('mongoose');
 
 const connectDB = async (DB_URL)=>{
-     return await mongoose.connect(DB_URL,{
+     try{
+          return await mongoose.connect(DB_URL,{
         dbName:"ngoguru",
           useNewUrlParser: true,
         useUnifiedTopology: true,
-        // serverApi: {
-        //     version: ServerApiVersion.v1,
-        //     strict: true,
-        //     deprecationErrors: true,
-        //   }
      }).then(()=>{
         console.log("DB Sucessfully Connected.")
-    }).catch((error)=>{
-        console.log("Error", error)
-
     })
+ }
+          catch(error){
+               console.log("Error", error)    
+          }
 }
 
 module.exports = connectDB
